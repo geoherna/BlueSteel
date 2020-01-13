2b82f364a6e1e9ec5e9974c338b9f0bd3522f363
George <geoherna@outlook.com>
2020-01-13 01:49:42 -0500

    ensures bytes is not empty

--- Sources/AvroDecoder.swift
+++ Sources/AvroDecoder.swift
@@ -115,10 +115,12 @@ open class AvroDecoder {
     open func decodeBytes() -> [UInt8]? {
         if let sizeLong = decodeLong() {
             let size = Int(sizeLong)
-            if size <= Int(bytes.count)-1 && size != 0 {
+            if bytes.count > 0 {
+                if size <= Int(bytes.count) && size != 0 {
                 let tmp = bytes[0..<size]
                 bytes.removeSubrange(0..<size)
                 return [UInt8](tmp)
+                }
             }
         }
         return nil
